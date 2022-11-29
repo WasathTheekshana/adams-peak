d4438dc407a8b52aa6b7f55bf49f557b017e4023
WasathTheekshana <wasath.vt@gmail.com>
2022-11-30 00:13:33 +0530

    dev: Created File Structure

--- Pages/routes.php
+++ Pages/routes.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sri Padaya</title>
+    <title>Routes</title>
+    <link rel="stylesheet" href="../global.css">
 </head>
 <body>
+
+    <?php include '../Footer/footer.html'; ?>
     
 </body>
 </html>
