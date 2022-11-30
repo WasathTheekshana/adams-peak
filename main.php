559b7f7ee97c8f13b3a3237e0d335ea65d32b9c9
WasathTheekshana <wasath.vt@gmail.com>
2022-11-30 17:39:17 +0530

    dev: Install node modules and tailwind

--- main.php
+++ main.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sri Padaya</title>
+    <link href="/dist/output.css" rel="stylesheet">
 </head>
 <body>
+
+<h1 class="text-3xl font-bold underline text-center">
+    Hello world!
+  </h1>
     
 </body>
 </html>
