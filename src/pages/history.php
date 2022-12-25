5f51e9a2e51933ecf4928645e64adc6be9501585
WasathTheekshana <wasath.vt@gmail.com>
2022-12-25 12:11:24 +0530

    fix: changes

--- src/pages/history.php
+++ src/pages/history.php
@@ -5,11 +5,98 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>History</title>
-    <link rel="stylesheet" href="history.css">
+    <link rel="stylesheet" href="./history.css">    
 </head>
 <body>
-  <?php include './../navBar/navBar.html' ?>
+<?php include '../navBar/navBar.php';?> 
 
-  <?php include './../Footer/footer.html' ?>
+  <div class="historypage">
+    <div class="cover">
+        <img src="historycoverpic.svg">
+        <div class="History">
+            <span>History</span>
+        </div>
+    </div>
+    <div class="row1">
+        <div class="row1-col1">
+            <div class="description1">
+                <p>Adam's Peak is a 2,243m tall mountain. It is well known for the Sri Pada, 
+                    "sacred footprint", a 1.8 m rock formation near the summit, which in Buddhist 
+                    tradition is held to be footprint of Buddha. Portuguese Christians believed 
+                    St Thomas and the eunuch of Candace, queen of Ethiopia. The footstep is covered 
+                    by a handsome roof, and is guarded by the priests of a rich monastery half-way 
+                    up the mountain, who maintain a shrine on the summit of the peak.</p>
+
+                <p>The mountain is most often scaled from December to May. During other months it 
+                    is hard to climb the mountain due to very heavy rain, extreme wind, and thick 
+                    mist. A Pali poem "Samantha Kuta Vannama" by a monk named Vedeha in the 13th 
+                    century confirms the increasing interest shown by the Sinhala-Buddhists to the 
+                    cult of this Footprint. In our recorded history, a good number of ancient kings 
+                    have visited the mountain from time to time.</p>
+            </div>
+        </div>
+        <div class="row1-col2">
+            <img src="historypic1.svg">
+        </div>
+    </div>
+    <div class="row2">
+        <div class="row2-col1">
+            <img src="historypic1.svg">
+        </div>
+        <div class="row2-col2">
+            <div class="description2">
+                <p>As indicated by legend, a long time back, Sri Lanka was a radically better place. 
+                    Around then, our nation had been tormented by devils, or "Yakshayo", who threatened 
+                    and butchered our predecessors for their tomfoolery and cheerfulness. Individuals 
+                    didn't aerobics of their homes because of a paranoid fear of being caught by them, 
+                    and no little kid could escape from their malicious hooks. Those were dull times…</p>
+
+                <p>Lord Buddha, after seeing this, quickly set out to free our country from this threat 
+                    and showed up in Mahiyanganaya to vanquish the Demon. The story goes that lord Buddha 
+                    made a whole island called "Giri Divayina" for the evil presences to live in and moved 
+                    it far away from Sri Lanka so individuals could live in harmony. It was opportunity as 
+                    they had never known…</p>
+            </div>
+        </div>
+    </div>
+    <div class="row3">
+        <div class="row3-col1">
+            <div class="description3">
+                <p>A lesson was conveyed by Lord Buddha following this occasion, which happened to the 
+                    ears of Lord Saman (or Sumana Saman as he is some of the time known), a divine being 
+                    possessing a close by mountain. He was so overwhelmed with feeling and appreciation 
+                    that he welcomed Lord Buddha to visit his mountain, and to put Lord Buddha's impression 
+                    there for of gift the country. However, this mountain was more modest than Adam's Peak. 
+                    Mother earth, after discovering this, had felt that it wasn't ideal for somebody as 
+                    radiant and ethical as the Buddha to influence such a modest spot. So as the story proceeds, 
+                    she had extended a picture of a dead snake structure Lord Saman's mountain highest point.</p>
+
+                <p>This mountain is referred to the present time as "Kunu diya parvathaya", where "Kunu" or 
+                    "Kuna" signifies "body". So, it fundamentally means "Body Rock".</p>
+            </div>
+        </div>
+        <div class="row3-col2">
+            <img src="/public/historypic1.svg">
+        </div>
+    </div>
+    <div class="row4">
+        <div class="row4-col1">
+            <img src="/public/historypic1.svg">
+        </div>
+        <div class="row4-col2">
+            <div class="description4">
+                <p>Taking the picture to be an evil sign, God Saman then, at that point, welcomed lord Buddha 
+                    to put his impression on the close by Adam's Peak, which was more noteworthy with respect 
+                    to the previous. Thus, the name “Sri Pada” was coined, where it translates to “Sacred Footprint”. 
+                    God Saman is currently said to monitor Sri Pada and consequently the expression "Samanala Kanda" 
+                    is likewise used to allude to Adam's Peak.</p>
+
+                <p>Concerning how it got the name "Adam's Peak", the Christian and Islamic religions guarantee it to 
+                    be where Adam originally set foot on earth subsequent to being projected down from the sky.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include '../footer/footer.php';?> 
 </body>
 </html>
