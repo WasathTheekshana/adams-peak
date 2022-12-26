2eedc74cdd415d89c5a07c007c5febe538c5da0d
yasithranusha <yasithranusha24@gmail.com>
2022-12-26 19:05:36 +0530

    Add sql file to create database & table for forum

--- src/components/forum/save.php
+++ src/components/forum/save.php
@@ -4,7 +4,7 @@ $id = test_input($_POST['id']);
 $name = test_input($_POST['name']);
 $msg = test_input($_POST['msg']);
 if($name != "" && $msg != ""){
-	$sql = $conn->query("INSERT INTO discussion (parent_comment, name, comment)
+	$sql = $conn->query("INSERT INTO experience (parent_comment, name, comment)
 			VALUES ('$id', '$name', '$msg')");
 	echo json_encode(array("statusCode"=>200));
 }
