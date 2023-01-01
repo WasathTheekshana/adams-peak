b490af300423ea2ed24f69f3887a00a02015db35
yasithranusha <yasithranusha24@gmail.com>
2023-01-01 23:29:50 +0530

    Change Time zone name

--- src/components/forum/forum.js
+++ src/components/forum/forum.js
@@ -12,13 +12,13 @@ success: function(data) {
     for (var i=0; i<data.length; i++) {
         var commentId = data[i].id;
         if(data[i].parent_comment == 0){
-        var row = $('<tr><td class="mainComment"><p class="commenter"><img class="avatar" src="./src/components/forum/avatar.jpg" width="30px" height="30px"/>' + data[i].name + ' <i class="date"> '+ data[i].date + '(IST)</i></p></br><p class="commentText">' + data[i].comment + '</br></br><a data-toggle="modal" data-id="'+ commentId +'" title="Add this item" class="open-ReplyModal" href="#ReplyModal">Reply</a>'+'</p></td></tr>');
+        var row = $('<tr><td class="mainComment"><p class="commenter"><img class="avatar" src="./src/components/forum/avatar.jpg" width="30px" height="30px"/>' + data[i].name + ' <i class="date"> '+ data[i].date + '(GMT)</i></p></br><p class="commentText">' + data[i].comment + '</br></br><a data-toggle="modal" data-id="'+ commentId +'" title="Add this item" class="open-ReplyModal" href="#ReplyModal">Reply</a>'+'</p></td></tr>');
         $('#record').append(row);
         for (var r = (data.length-1); (r >= 0); r--)
                 {
                     if ( data[r].parent_comment == commentId)
                     {
-                        var comments = $('<tr><td class="repComment"><p class="commenter"><img class="avatar" src="./src/components/forum/avatar.jpg" width="30px" height="30px"/>' + data[r].name + ' <i class="date"> ' + data[r].date + '(IST)</i></p></br><p class="commentText">'+ data[r].comment +'</p></td></tr>');
+                        var comments = $('<tr><td class="repComment"><p class="commenter"><img class="avatar" src="./src/components/forum/avatar.jpg" width="30px" height="30px"/>' + data[r].name + ' <i class="date"> ' + data[r].date + '(GMT)</i></p></br><p class="commentText">'+ data[r].comment +'</p></td></tr>');
                         $('#record').append(comments);
                     }
                 }$('#record').append($('<tr class="commentGap"></tr>'));
